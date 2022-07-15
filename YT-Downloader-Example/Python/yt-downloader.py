1a826ba5f8d51422d7c485f07c7cb4d1ec75f562
zlElo <108819252+zlElo@users.noreply.github.com>
2022-07-15 14:31:13 +0200

    Create a new repo
    I create an example for a simple youtube downloader

---
+++ YT-Downloader-Example/Python/yt-downloader.py
@@ -0,0 +1,29 @@
+from pytube import YouTube
+import time
+
+#time
+items = list(range(0, 57))
+l = len(items)
+
+#link question
+linkquestion = input("Enter the link of YouTube video you want to download:  ")
+yt = YouTube(linkquestion)
+
+#details
+print('---------------------------------------------------------------------------------')
+print("Title: ",yt.title)
+print("Views: ",yt.views)
+print("Rating: ",yt.rating)
+print('---------------------------------------------------------------------------------')
+
+#Getting highest res.
+ys = yt.streams.get_highest_resolution()
+
+print('The download starts.')
+time.sleep(1)
+print('The download starts..')
+time.sleep(1)
+print('the downloads starts...')
+time.sleep(1)
+ys.download()
+print('Your video is downloaded!')
